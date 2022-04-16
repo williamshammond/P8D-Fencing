2170626933194edc25ab3618dc22cbe1922d2714
Elijah Temwani Zulu <modeaxe@Gmail.com>
2022-04-16 02:13:29 -0400

    quiz questions added

---
+++ static/data.js
@@ -0,0 +1,18 @@
+lessons = [
+    "1" : {
+    "id":"1",
+    "title":"The Wind Rises",
+    "media":"https://upload.wikimedia.org/wikipedia/en/a/a3/Kaze_Tachinu_poster.jpg",
+    "director":"Hayao Miyazaki",
+    "year":"2013",
+    "synopsis":"A lifelong love of flight inspires Japanese aviation engineer Jiro Horikoshi (Hideaki Anno), whose storied career includes the creation of the A6M World War II fighter plane.",
+    "podcast":"<iframe src=\"https://embed.acast.com/$/9b41cd90-c759-4ccb-98b1-16a633915c57/thewindrises-ghibliotheque-17\" frameBorder=\"0\" width=\"100%\" height=\"110px\" allow=\"autoplay\"></iframe>",
+    "similar":["2", "10","3"]
+    },
+
+]
+
+quiz = [
+
+
+]
