ea2f1cec5409a681ebfe0bdef8993bd8c3c806b6
--replace-all <71682563+williamshammond@users.noreply.github.com>
2022-04-16 21:58:02 -0400

    Add new navigation structure, change home styling
    Change navigation structure to return home at the start and end of each learning section.

--- static/learn.js
+++ static/learn.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
     let lessonNumber = lesson["lesson_id"];
     console.log(lessonNumber);
 
-    let prevText = lessonNumber != 1 ? "Previous": "Return Home";
-    let prevLink = lessonNumber != 1 ? `/learn/${+lessonNumber - 1}`: `/`;
+    let prevText = (lessonNumber != 1 && lessonNumber != 9 && lessonNumber != 14) ? "Previous": "Return Home";
+    let prevLink = (lessonNumber != 1 && lessonNumber != 9 && lessonNumber != 14) ? `/learn/${+lessonNumber - 1}`: `/`;
 
-    let nextText = (lessonNumber != 8 && lessonNumber != 13 && lessonNumber != 20)? "Next": "Return Home";
-    let nextLink = (lessonNumber != 8 && lessonNumber != 13 && lessonNumber != 20)? `/learn/${+lessonNumber + 1}`: `/`;
+    let nextText = (lessonNumber != 8 && lessonNumber != 13 && lessonNumber != 20) ? "Next": "Return Home";
+    let nextLink = (lessonNumber != 8 && lessonNumber != 13 && lessonNumber != 20) ? `/learn/${+lessonNumber + 1}`: `/`;
 
     $("#prevButton").append(`<a href="${prevLink}" class="btn btn-primary btn-lg gapTop" role="button" aria-disabled="true">${prevText}</a>`);
     $("#nextButton").append(`<a href="${nextLink}" class="btn btn-primary btn-lg gapTop" role="button" aria-disabled="true">${nextText}</a>`);
